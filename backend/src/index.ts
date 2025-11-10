2bfd563dc093623d8e09f666b414e35cc363f2bf
PrithiviPM2580 <prithivipemimagar>
2025-11-10 21:49:03 +0545

    Set up initial Express server with environment configuration and necessary dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,3 @@
-console.log("Hello Prithivi");
+import startServer from "server.js";
+
+startServer();
---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import app from "app.js";
+
+const PORT = 3000;
+
+const startServer = async () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+};
+
+export default startServer;
